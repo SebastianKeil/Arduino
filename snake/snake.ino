0abe556f7d1f35a7487d4a9176f8acb2fab3bb5a
Glenn Dittmann <57635813+glennDittmann@users.noreply.github.com>
2020-10-24 23:33:47 +0200

    update to class version

--- snake/snake.ino
+++ snake/snake.ino
@@ -2,7 +2,7 @@
 
 #define PIN 6
 #define NUM_PIXELS 450
-#define PIXEL_WIDTH 30
+#define PIXEL_WIDTH 30  
 #define PIXEL_LENGTH 15
 
 #define UP_BUTTON 2
@@ -15,44 +15,43 @@
 #define RIGHT 2
 #define LEFT 3
 
+#define START_X 10
+#define START_Y 7
 #define START_LEN 4 //length of tail, without head
 #define MAX_TAIL_LENGTH 16
 
-//TODO: delete fruit when new spawns == when it gets eaten
-//TODO: make snake bigger after eating fruit
-//Todo: Make sure the fruit can't spawn on the snake
+//TODO: check inputs makes mistake, e.g. we can still go left, when already going right 
+//TODO: Make sure the fruit can't spawn on the snake
+// something like while(fruit.pos == head || tail[1] || ...) do ( fruit.pos = rand(0,499)
+//TODO: start of game screen
+// display game name, start message (and high score)
 //TODO: end of game screen
+// display game over, number of points, and restart message
 
-Adafruit_NeoPixel strip(NUM_PIXELS, PIN, NEO_GBR);
-
-int head_x;
-int head_y;
-int tail[MAX_TAIL_LENGTH];   //pixel number, not coordinates
-int len;
-int fruit;
-int dir = RIGHT;
-
-class board {
-
-};
-
-class snake {
-    int head_x, head_y;
-    int len;            //length of tail, without head
-    int tail [MAX_TAIL_LENGTH];       //TODO: add max_length which is given in constructor and is used instead of hard coded 16
+class Snake {
+    public: int head_x, head_y;
+    int len;                            //length of tail, without head
+    public: int tail [MAX_TAIL_LENGTH];         //TODO: add max_length which is given in constructor and is used instead of hard coded 16
+    int removedTailSegment;
     int dir;
     int head_color[3] = {0, 60, 255};
     int tail_color[3] = {150, 20, 255};
 
 public:
-    snake(int x, int y, int start_len){
+    Snake(int x, int y, int start_len){
         this->head_x = x;
         this->head_y = y;
         this->len = start_len;
+        this->tail[0] = coordsToPixelNum(this->head_x-1, this->head_y);
+        for (int i=1; i<this->len; i++){
+            this->tail[i] = this->coordsToPixelNum(this->head_x-(i+1), this->head_y);
+        }
         this->dir = RIGHT;
     };
 
     void advance() {
+        //store the pixel number, where to turn of the color
+        this->removedTailSegment = this->tail[len-1];
         //iterate over the snake from back to front, so:
         //i:     0  1  2  3   4-15 | len is 4
         //  [32][33,34,35,36, NUll] becomes
@@ -62,23 +61,28 @@ public:
         }
         // set first tail segment to head coordinates:
         // [32][32,33,34,35, NULL]
-        this->tail[0] = coordsToPixelNum(this->head_x, this->head_y);
+        this->tail[0] = this->headPixelNum();
 
-        //update head coordinates
+        //check inputs and then update head coordinates
+        //this->checkInputs();
         switch (this->dir) {
             case UP:
-                head_y -= 1;
+                this->head_y -= 1;
                 break;
             case DOWN:
-                head_y += 1;
+                this->head_y += 1;
                 break;
             case RIGHT:
-                head_x += 1;
+                this->head_x += 1;
                 break;
             case LEFT:
-                head_x -= 1;
+                this->head_x -= 1;
                 break;
         }
+    }
+
+    void eatFruit(){
+        this->len++; 
     }
 
     bool isDead() {
@@ -87,24 +91,36 @@ public:
             return true;
         }
         //check if snake hit itself
-        for (int i = 0; i < len; i++){
-            if (tail[i] == coordsToPixelNum(this->head_x, this->head_y)) return true;
+        for (int i = 0; i < this->len; i++){
+            if (tail[i] == this->headPixelNum()) return true;
         }
         return false;
     }
 
-    void removePixel(Adafruit_NeoPixel strip){
-        strip.setPixelColor(coordsToPixelNum(this->head_x, this->head_y), 0, 0, 0);
+    void removePixel(Adafruit_NeoPixel * strip){                                       //give a pointer to our strip object
+        strip->setPixelColor(this->headPixelNum(), 0, 0, 0);
         for (int i = 0; i < len; i++) {
-            strip.setPixelColor(tail[i], 0, 0, 0);
+            strip->setPixelColor(tail[i], 0, 0, 0);
         }
     }
 
-    void setPixel(Adafruit_NeoPixel strip) {
-        strip.setPixelColor(coordsToPixelNum(this->head_x, this->head_y), 0, 60, 255);
-        for (int i = 0; i < len; i++) {
-            strip.setPixelColor(tail[i], 150, 20*i, 255);
+    void resetPosition(){
+        this->head_x = START_X;
+        this->head_y = START_Y;
+        this->len = START_LEN;
+        this->tail[0] = this->coordsToPixelNum(this->head_x-1, this->head_y);
+        for (int i=1; i<this->len; i++){
+            this->tail[i] = this->coordsToPixelNum(this->head_x-(i+1), this->head_y);
         }
+        this->dir = RIGHT;
+    }
+
+    void setPixel(Adafruit_NeoPixel * strip) {
+        strip->setPixelColor(this->headPixelNum(), 0, 60, 255);
+        for (int i = 0; i < this->len; i++) {
+            strip->setPixelColor(this->tail[i], 150, 20*i, 255);
+        }
+        strip->setPixelColor(this->removedTailSegment, 0, 0, 0);
     }
 
     void checkInputs(){
@@ -121,15 +137,19 @@ public:
     int coordsToPixelNum(int x, int y) {
         int pixelNumber;
         if (y % 2 == 0) {
-            pixelNumber = (y+1) * 30 - x -1;
+            pixelNumber = (y+1) * 30 - x - 1;
         } else {
             pixelNumber = y * 30 + x;
         }
         return pixelNumber;
     }
+
+    int headPixelNum() {
+        return this->coordsToPixelNum(this->head_x, this->head_y);
+    }
 };
 
-class fruit {
+class Fruit {
     int position;
     int color[3] = {255, 255, 0};
 
@@ -137,18 +157,48 @@ class fruit {
     //ideas: move fruit und time fruit
 
 public:
-    fruit(){
-        this->position = (int) random(NUM_PIXELS-1);;
+    Fruit(){
+        this->position = (int) random(NUM_PIXELS-1);
     }
 
-    void setPixel(Adafruit_NeoPixel strip){
-        strip.setPixelColor(this->position, color[0], color[1], color[2]);
+    void resetPosition(){
+        this->position = (int) random(NUM_PIXELS-1);
     }
 
-    void remove(Adafruit_neoPixel strip){
-        strip.setPixelColor(this->position, 0, 0, 0);
+    void setPixel(Adafruit_NeoPixel * strip){
+        strip->setPixelColor(this->position, color[0], color[1], color[2]);
+    }
+
+    void removePixel(Adafruit_NeoPixel * strip){
+        strip->setPixelColor(this->position, 0, 0, 0);
+    }
+
+    bool isEaten(Snake * snake) {
+        if (snake->headPixelNum() == this->position) return true;
+        return false;
     }
 };
+
+// game objects
+Adafruit_NeoPixel strip(NUM_PIXELS, PIN, NEO_GBR);
+            Snake snake(START_X, START_Y, START_LEN);
+            Fruit fruit;
+
+//pointers to these objects
+Adafruit_NeoPixel * stripPtr = &strip;
+            Snake * snakePtr = &snake;
+            Fruit * fruitPtr = &fruit;
+
+void resetGame(Snake * snake, Fruit * fruit, Adafruit_NeoPixel * strip){
+    snake->resetPosition();
+    snake->setPixel(strip);
+    
+    fruit->resetPosition();
+    fruit->setPixel(strip);
+    strip->show(); 
+    
+}
+
 
 void setup() {
     Serial.begin(9600);
@@ -158,131 +208,29 @@ void setup() {
     pinMode(LEFT_BUTTON, INPUT_PULLUP);
  
     strip.begin();
-    blackScreen();
+    strip.clear();
     strip.show();
-    resetGame();
-    strip.show();
+    resetGame(snakePtr, fruitPtr, stripPtr);
     randomSeed(analogRead(0));
 }
 
+
 void loop() {
-    if (SnakeIsDead()) {
-        deleteSnake();
-        deleteFruit();
-        resetGame();
+    snake.advance();
+    snake.setPixel(stripPtr);
+    if( snake.isDead() ) {
+        snake.removePixel(stripPtr);
+        fruit.removePixel(stripPtr);
+        resetGame(snakePtr, fruitPtr, stripPtr);
     }
-    checkInputs();
-    advanceSnake();
-    setSnakePixel();
-    if (fruitIsEaten()){
-        setFruitPixel();
-        len++;
+    if( fruit.isEaten(snakePtr) ){
+        fruit.removePixel(stripPtr);
+        fruit.resetPosition();
+        snake.eatFruit();
+        snake.setPixel(stripPtr);
+        fruit.setPixel(stripPtr);
     }
 
     strip.show();
     delay(200);
-}
-
-void checkInputs(){
-    if (digitalRead(UP_BUTTON) && (dir == LEFT || dir == RIGHT))
-        dir = UP;
-    if (digitalRead(DOWN_BUTTON) && (dir == LEFT || dir == RIGHT))
-        dir = DOWN;
-    if (digitalRead(RIGHT_BUTTON) && (dir == UP || dir == DOWN))
-        dir = RIGHT;
-    if (digitalRead(LEFT_BUTTON) && (dir == UP || dir == DOWN))
-        dir = LEFT;
-}
-
-void advanceSnake() {
-    //iterate over the snake from back to front, so:
-    //i:     0  1  2  3   4-15 | len is 4
-    //  [32][33,34,35,36, NUll] becomes
-    //  [32][33,33,34,35, NULL]
-    for (int i = len-1; i > 0; i--){
-        tail[i] = tail[i-1];
-    }
-    // set first tail segment to head coordinates:
-    // [32][32,33,34,35, NULL]
-    tail[0] = coordsToPixelNum(head_x, head_y);
-
-    //update head coordinates
-    switch (dir) {
-        case UP:
-          head_y -= 1;
-          break;
-        case DOWN:
-          head_y += 1;
-          break;
-        case RIGHT:
-          head_x += 1;
-          break;
-        case LEFT:
-          head_x -= 1;
-          break;
-      }
-}
-
-bool SnakeIsDead() {
-    //check if head left the screen
-    if (head_x < 0 || head_x > 29 || head_y < 0 || head_y > 14) {
-    return true;
-    }
-    //check if snake hit itself
-    for (int i = 0; i < len; i++){
-        if (tail[i] == coordsToPixelNum(head_x, head_y)) return true;
-    }
-    return false;
-}
-
-void deleteSnake(){
-  strip.setPixelColor(coordsToPixelNum(head_x, head_y), 0, 0, 0);
-  for (int i = 0; i < len; i++) {
-    strip.setPixelColor(tail[i], 0, 0, 0);
-  }
-}
-
-void resetGame(){
-    head_x = 15;
-    head_y = 7;
-    len = START_LEN;
-    for(int i = 0; i < len; i++){
-        tail[i] = coordsToPixelNum(15, head_y-1-i);
-    }
-    dir = RIGHT;
-    setSnakePixel();
-    delay(200);
-}
-
-void setSnakePixel() {
-  strip.setPixelColor(coordsToPixelNum(head_x, head_y), 0, 60, 255);
-  for (int i = 0; i < len; i++) {
-    strip.setPixelColor(tail[i], 150, 20*i, 255);
-  }
-}
-
-int coordsToPixelNum(int x, int y) {
-  int pixelNumber;
-
-  if (y % 2 == 0) {
-    pixelNumber = (y+1) * 30 - x -1;
-  } else {
-    pixelNumber = y * 30 + x;
-  }
-  return pixelNumber;
-}
-
-void blackScreen(){
-  strip.clear();
-  strip.show();
-
-}
-
-void setFruitPixel() {
-    fruit = (int) random(NUM_PIXELS-1);
-    strip.setPixelColor(fruit, 255, 255, 0);
-}
-
-void deleteFruit(){
-    strip.setPixelColor(fruit, 0, 0, 0);
 }
